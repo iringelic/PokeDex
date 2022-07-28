ca28d9b0bdbcccbb2f9b0ce14d0eaec4a98a8738
GreatGeneral <hamzamohamed113@gmail.com>
2022-07-28 16:45:03 +0100

    My react app worked!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [pokemons, setPokemon] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const {data} = await axios.get(`https://pokeapi.co/api/v2/pokemon/`)
+                setPokemon(data.results)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        fetchData()
+    }, [])
+
+    return (
+      <div className="App">
+        <div className="results">
+          {pokemons.map((pokemon) => (
+            <div key={pokemon.id}>
+              <img src={pokemon.image} alt={pokemon.name} />
+              {pokemon.name}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
 }
 
 export default App;
